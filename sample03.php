ed8c0c5a733707e382e913f5a5a17e646329bfc7
tienhai1989 <tienhai1989@gmail.com>
2015-05-13 04:35:14 +0000

    0513 practice

---
+++ sample03.php
@@ -0,0 +1,19 @@
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+       <?php
+      /*echo('現在は' . date('G時 i分 s秒') . 'です');
+      */
+      print(date('G'));
+      print('時');
+      print(date('i'));
+      print('分');
+      print(date('s'));
+      print('秒');
+       ?>
+    </body>
+    </html>
